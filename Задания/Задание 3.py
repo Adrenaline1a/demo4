d3b2b62e2b02fa4b60b1ed99aeb92d5804b9dc48
Adrenaline1 <zligostev.igor123@yandex.ru>
2021-04-20 17:35:35 +0300

    Третье задание

--- Задания/Задание 3.py
+++ Задания/Задание 3.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import math
+x=0
 if __name__ == '__main__':
+    for i in range(100):
+        if (i>20 and i%3==0):
+            x+=i
+print("Сумма чисел, удовлетворяющих условиям, равна:", x)
