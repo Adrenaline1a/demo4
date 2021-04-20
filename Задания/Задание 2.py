68b03084e0f977adf010ec7e3ecc7ca873452716
Adrenaline1 <zligostev.igor123@yandex.ru>
2021-04-20 16:48:26 +0300

    второе задание + схема

--- Задания/Задание 2.py
+++ Задания/Задание 2.py
@@ -5,12 +5,14 @@ if __name__ == '__main__':
     a = float(input("Введите число а:"))
     b = float(input("Введите число b:"))
     c = float(input("Введите число c:"))
-    a = math.fabs(a)
-    b = math.fabs(b)
-    c = math.fabs(c)
-    list = [a, b, c]
-    for i in list:
-        if i>=4:
-            print(i)
-        else:
-            print('Не удовлетворяет условию')
+    a1 = math.fabs(a)
+    b1 = math.fabs(b)
+    c1= math.fabs(c)
+    if a1>=4:
+        print("a=", a)
+    if b1>=4:
+        print("b=", b)
+    if c1>=4:
+        print("c=", c)
+    else:
+        print("Нет чисел, удовлетворяющих условию")
