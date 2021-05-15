6dc2ba7df4ebfec01b077d95c29dc0bb32cd08cf
Adrenaline1 <zligostev.igor123@yandex.ru>
2021-05-15 14:37:40 +0300

    четвёртое задание

--- Задания/Повышенное.py
+++ Задания/Повышенное.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import math
-import sys
 EPS = 1e-10
 if __name__ == '__main__':
     x = float(input("Value of x? "))
-    if x == 0:
-        print("Illegal value of x", file=sys.stderr)
-        exit(1)
-    a = (x**2)/2
-    S, n = a, 1
+    a = x
+    S, n = a, 0
     while math.fabs(a) > EPS:
-        a *= (-1*x**2)/((2*n+3)*(2*n+1))
+        a *= (-1*(x**2)*(2*n+1))/(((2*n+3)**2)*(2*n+2))
         S += a
         n += 1
     print(f"Si({x})={S}")
